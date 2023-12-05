7a0286b083456f42a970529abfdcfdcce0016f90
Irfan <irfantayyib07@gmail.com>
2023-12-05 20:11:59 +0500

    formatted

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from './api/apiSlice';
 
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
+ reducer: {
+  [apiSlice.reducerPath]: apiSlice.reducer,
+ },
+ middleware: getDefaultMiddleware =>
+  getDefaultMiddleware().concat(apiSlice.middleware),
+ devTools: true
 })
--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -3,46 +3,46 @@ import User from './User'
 
 const UsersList = () => {
 
-    const {
-        data: users,
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    } = useGetUsersQuery()
+ const {
+  data: users,
+  isLoading,
+  isSuccess,
+  isError,
+  error
+ } = useGetUsersQuery()
 
-    let content
+ let content
 
-    if (isLoading) content = <p>Loading...</p>
+ if (isLoading) content = <p>Loading...</p>
 
-    if (isError) {
-        content = <p className="errmsg">{error?.data?.message}</p>
-    }
+ if (isError) {
+  content = <p className="errmsg">{error?.data?.message}</p>
+ }
 
-    if (isSuccess) {
+ if (isSuccess) {
 
-        const { ids } = users
+  const { ids } = users
 
-        const tableContent = ids?.length
-            ? ids.map(userId => <User key={userId} userId={userId} />)
-            : null
+  const tableContent = ids?.length
+   ? ids.map(userId => <User key={userId} userId={userId} />)
+   : null
 
-        content = (
-            <table className="table table--users">
-                <thead className="table__thead">
-                    <tr>
-                        <th scope="col" className="table__th user__username">Username</th>
-                        <th scope="col" className="table__th user__roles">Roles</th>
-                        <th scope="col" className="table__th user__edit">Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableContent}
-                </tbody>
-            </table>
-        )
-    }
+  content = (
+   <table className="table table--users">
+    <thead className="table__thead">
+     <tr>
+      <th scope="col" className="table__th user__username">Username</th>
+      <th scope="col" className="table__th user__roles">Roles</th>
+      <th scope="col" className="table__th user__edit">Edit</th>
+     </tr>
+    </thead>
+    <tbody>
+     {tableContent}
+    </tbody>
+   </table>
+  )
+ }
 
-    return content
+ return content
 }
 export default UsersList
