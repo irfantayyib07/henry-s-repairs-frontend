1e7c3f4ba556146dc87a2e7b229103a18a177533
Irfan <irfantayyib07@gmail.com>
2023-12-05 23:51:23 +0500

    formatted

--- src/App.js
+++ src/App.js
@@ -14,37 +14,37 @@ import Prefetch from './features/auth/Prefetch'
 import PersistLogin from './features/auth/PersistLogin';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
+ return (
+  <Routes>
+   <Route path="/" element={<Layout />}>
+    <Route index element={<Public />} />
+    <Route path="login" element={<Login />} />
 
-        <Route element={<PersistLogin />}>
-          <Route element={<Prefetch />}>
-            <Route path="dash" element={<DashLayout />}>
+    <Route element={<PersistLogin />}>
+     <Route element={<Prefetch />}>
+      <Route path="dash" element={<DashLayout />}>
 
-              <Route index element={<Welcome />} />
+       <Route index element={<Welcome />} />
 
-              <Route path="users">
-                <Route index element={<UsersList />} />
-                <Route path=":id" element={<EditUser />} />
-                <Route path="new" element={<NewUserForm />} />
-              </Route>
+       <Route path="users">
+        <Route index element={<UsersList />} />
+        <Route path=":id" element={<EditUser />} />
+        <Route path="new" element={<NewUserForm />} />
+       </Route>
 
-              <Route path="notes">
-                <Route index element={<NotesList />} />
-                <Route path=":id" element={<EditNote />} />
-                <Route path="new" element={<NewNote />} />
-              </Route>
+       <Route path="notes">
+        <Route index element={<NotesList />} />
+        <Route path=":id" element={<EditNote />} />
+        <Route path="new" element={<NewNote />} />
+       </Route>
 
-            </Route>{/* End Dash */}
-          </Route>
-        </Route>
+      </Route>{/* End Dash */}
+     </Route>
+    </Route>
 
-      </Route>
-    </Routes>
-  );
+   </Route>
+  </Routes>
+ );
 }
 
 export default App;
--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -11,53 +11,53 @@ const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
 
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
+ const navigate = useNavigate()
+ const { pathname } = useLocation()
 
-    const [sendLogout, {
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    }] = useSendLogoutMutation()
+ const [sendLogout, {
+  isLoading,
+  isSuccess,
+  isError,
+  error
+ }] = useSendLogoutMutation()
 
-    useEffect(() => {
-        if (isSuccess) navigate('/')
-    }, [isSuccess, navigate])
+ useEffect(() => {
+  if (isSuccess) navigate('/')
+ }, [isSuccess, navigate])
 
-    if (isLoading) return <p>Logging Out...</p>
+ if (isLoading) return <p>Logging Out...</p>
 
-    if (isError) return <p>Error: {error.data?.message}</p>
+ if (isError) return <p>Error: {error.data?.message}</p>
 
-    let dashClass = null
-    if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
-        dashClass = "dash-header__container--small"
-    }
+ let dashClass = null
+ if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+  dashClass = "dash-header__container--small"
+ }
 
-    const logoutButton = (
-        <button
-            className="icon-button"
-            title="Logout"
-            onClick={sendLogout}
-        >
-            <FontAwesomeIcon icon={faRightFromBracket} />
-        </button>
-    )
+ const logoutButton = (
+  <button
+   className="icon-button"
+   title="Logout"
+   onClick={sendLogout}
+  >
+   <FontAwesomeIcon icon={faRightFromBracket} />
+  </button>
+ )
 
-    const content = (
-        <header className="dash-header">
-            <div className={`dash-header__container ${dashClass}`}>
-                <Link to="/dash">
-                    <h1 className="dash-header__title">techNotes</h1>
-                </Link>
-                <nav className="dash-header__nav">
-                    {/* add more buttons later */}
-                    {logoutButton}
-                </nav>
-            </div>
-        </header>
-    )
+ const content = (
+  <header className="dash-header">
+   <div className={`dash-header__container ${dashClass}`}>
+    <Link to="/dash">
+     <h1 className="dash-header__title">techNotes</h1>
+    </Link>
+    <nav className="dash-header__nav">
+     {/* add more buttons later */}
+     {logoutButton}
+    </nav>
+   </div>
+  </header>
+ )
 
-    return content
+ return content
 }
 export default DashHeader
