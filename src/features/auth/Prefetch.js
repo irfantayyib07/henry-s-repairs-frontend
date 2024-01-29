0fb59ec6697363f305fab646c0714ebf84550288
Irfan <irfantayyib07@gmail.com>
2024-01-29 23:31:20 +0500

    studied prefetch

--- src/features/auth/Prefetch.js
+++ src/features/auth/Prefetch.js
@@ -3,14 +3,22 @@ import { notesApiSlice } from '../notes/notesApiSlice'
 import { usersApiSlice } from '../users/usersApiSlice';
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useState } from "react";
 
 const Prefetch = () => {
+ // const [count, setCount] = useState(1);
+ // console.log(count);
 
  useEffect(() => {
   store.dispatch(notesApiSlice.util.prefetch('getNotes', 'notesList', { force: true }))
   store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true }))
  }, [])
 
- return <Outlet />
+ return (
+  <>
+   {/* <button onClick={() => setCount(prev => prev + 1)}>RENDER PREFETCH</button> */}
+   <Outlet />
+  </>
+ )
 }
 export default Prefetch
